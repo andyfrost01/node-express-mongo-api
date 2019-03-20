9c1798c2788a8d9dc00fd2dc3daaf4fb1b46bd32
Andy Frost <a.frost7785@gmail.com>
2019-03-20 22:28:59 +0000

    create server

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const router = require('./routes');
+
+const config = require('./config');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo-api',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+
+  app.use(router);
+  
+  
+
+  app.listen(config.port, () => console.log(`Server running on ${config.port}`));
